3ba8e9b9475072ab558c6b6b7b1127b40990c72d
VishalForGitHub <vishaldhokchaule.contact@gmail.com>
2024-11-20 09:00:29 -0300

    commit changes

--- src/routes.jsx
+++ src/routes.jsx
@@ -1,33 +1,46 @@
 import React, { useState } from "react";
-import {BrowserRouter as Router,Routes,Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import NavBar from "./component/NavBar";
 import Home from "./pages/Home";
 import SectionResult from "./pages/SectionResult";
 import ProductView from "./pages/ProductView";
-export default function Navigators(){
+export default function Navigators() {
 
-    let [cartCount,setCartCount]=useState([]);
-    let [floatingMessage,setFloatingMessage]=useState(false);
-
-    let handleCartCount=(id)=>{
+    let [cartCount, setCartCount] = useState([]);
+    let [floatingMessage, setFloatingMessage] = useState(false);
+    let isPresent = false;
+    let handleCartCount = (id) => {
         // setCartCount(cartCount ++);
         // if(cartCount.length==0)
-            setCartCount([...cartCount,id]);
-            setFloatingMessage(true);
+        let i;
+        for (i = 0; i < cartCount.length; i++) {
+            if (id == cartCount[i]) {
+                isPresent = true;
+                break;
+            }
+        }
 
-            setTimeout(()=>{
+        if (i == cartCount.length)
+            isPresent = false;
+
+        if (!isPresent) {
+            setCartCount([...cartCount, id]);
+            setFloatingMessage(true);
+            // isPresent=!isPresent;
+            setTimeout(() => {
                 setFloatingMessage(false)
-            },3000);
+            }, 3000);
+        }
     }
 
-    return(
+    return (
         <Router>
-            <NavBar cartCount={cartCount} floatingMessage={floatingMessage}/>
-             <Routes>
-                 <Route path="/" element={<Home handleCartCount={handleCartCount} floatingMessage={floatingMessage}/>}/>
-                 <Route path="/section/:sectionName" element={<SectionResult/>}/>
-                 <Route path="/product/:id" element={<ProductView/>}/>
-             </Routes>   
-        </Router>    
+            <NavBar cartCount={cartCount} floatingMessage={floatingMessage} />
+            <Routes>
+                <Route path="/" element={<Home handleCartCount={handleCartCount} floatingMessage={floatingMessage} />} />
+                <Route path="/section/:sectionName" element={<SectionResult />} />
+                <Route path="/product/:id" element={<ProductView />} />
+            </Routes>
+        </Router>
     )
 }
